46a9599681edefb4d04d24544bce50a1168205a5
Fictioncoding <160401128+Fictioncoding@users.noreply.github.com>
2026-04-18 20:14:32 +0530

    Update auth.ts
    base url change

--- src/client/api/auth.ts
+++ src/client/api/auth.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000/auth/organisation";
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/auth/organisation";
 
 // Types for API responses
 type AuthResponse = {
